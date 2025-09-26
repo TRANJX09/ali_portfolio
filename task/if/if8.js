b7085662058283be8b4b04c24800fee8e2cce6bf
TRANJX09 <alijonabdullozoda7@gmail.com>
2025-09-27 01:35:38 +0500

    задача if8 выполнено

---
+++ task/if/if8.js
@@ -0,0 +1,15 @@
+// If8◦
+// . Даны два числа. Вывести вначале большее, а затем меньшее из них.
+
+let a = 45;
+let b = 45;
+
+if (a > b) {
+  console.log(a);
+  console.log(b);
+} else if (b > a) {
+  console.log(b);
+  console.log(a);
+} else {
+  console.log("Оба равны");
+}
