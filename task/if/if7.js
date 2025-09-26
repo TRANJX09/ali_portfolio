bb8ad7cf1f8341ff878840328f6e9b0121c852e3
TRANJX09 <alijonabdullozoda7@gmail.com>
2025-09-27 01:26:19 +0500

    задача if7 выполнен

---
+++ task/if/if7.js
@@ -0,0 +1,12 @@
+// If7. Даны два числа. Вывести порядковый номер меньшего из них.
+
+let a = 20;
+let b = 20;
+
+if (a < b) {
+  console.log(a);
+} else if (b < a) {
+  console.log(b);
+} else {
+  console.log("Оба равны");
+}
