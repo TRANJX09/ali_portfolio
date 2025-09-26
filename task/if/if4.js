da4fa9066d2ae7ad263bc212f80706b99d67c99d
TRANJX09 <alijonabdullozoda7@gmail.com>
2025-09-27 01:09:20 +0500

    задача if4 выполнено

---
+++ task/if/if4.js
@@ -0,0 +1,21 @@
+// If4◦
+// . Даны три целых числа. Найти количество положительных чисел в исходном наборе.
+
+let a = 16;
+let b = 20;
+let c = 56;
+
+plusNumbers = 0;
+
+if (a > 0) {
+  plusNumbers = plusNumbers + 1;
+}
+
+if (b > 0) {
+  plusNumbers = plusNumbers + 1;
+}
+if (c > 0) {
+  plusNumbers = plusNumbers + 1;
+}
+
+console.log(plusNumbers);
