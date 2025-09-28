29c6753607084578bac731a186145c921ff44105
TRANJX09 <alijonabdullozoda7@gmail.com>
2025-09-28 12:00:32 +0500

    задача integer7 выполнено

---
+++ task/integer/integer7.js
@@ -0,0 +1,10 @@
+// Integer7◦
+// . Дано двузначное число. Найти сумму и произведение его цифр
+
+let a = 56;
+
+let sum = Math.floor(a / 10);
+let ost = a % 10;
+
+console.log(sum + ost);
+console.log(sum * ost);
