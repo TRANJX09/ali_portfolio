41903ad195d4fe048ca3aa18ce7246106da7ac9f
TRANJX09 <alijonabdullozoda7@gmail.com>
2025-09-30 22:32:55 +0500

    many commits

---
+++ practics/for_repeat.js
@@ -0,0 +1,20 @@
+// let a = "hello";
+
+// {
+//   console.log(a);
+//   var b = "Day is good";
+// }
+// console.log(b);
+
+let a = 45;
+{
+  let xz = 26;
+  {
+    console.log(xz);
+    {
+      var y = "Ali";
+    }
+  }
+}
+
+console.log(y);
